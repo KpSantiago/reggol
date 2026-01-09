e86901074984881a2e69d8401dc9e4bb1c6f0f83
KpSantiago <santiago.kauajose12@gmail.com>
2026-01-09 00:50:57 -0300

    fix: usar paralelismo ao invés de concorrência

--- src/main/java/me/kpsantiago/Reggol.java
+++ src/main/java/me/kpsantiago/Reggol.java
@@ -1,135 +1,54 @@
 package me.kpsantiago;
 
-import me.kpsantiago.config.ConnectionContext;
 import me.kpsantiago.config.SecureSocketConfig;
 import me.kpsantiago.syslog.SyslogMessage;
 
-import javax.net.ssl.*;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLSocket;
 import java.io.*;
-import java.net.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Reggol {
     public static void main(String[] args) {
         try (
-                ServerSocketChannel serverChannel = ServerSocketChannel.open();
-                Selector selector = Selector.open()
+                SSLServerSocket server = (SSLServerSocket) SecureSocketConfig.config().createServerSocket();
         ) {
-            serverChannel.configureBlocking(false);
-            serverChannel.bind(new InetSocketAddress("127.0.0.1", 888));
-            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+           server.setNeedClientAuth(true);
+           server.bind(new InetSocketAddress("127.0.0.1", 888));
+
+            ExecutorService pool = Executors.newFixedThreadPool(100);
 
             while (true) {
-                selector.select();
-                Set<SelectionKey> keys = selector.selectedKeys();
-                Iterator<SelectionKey> it = keys.iterator();
-
-                while (it.hasNext()) {
-                    SelectionKey key = it.next();
-                    it.remove();
-
-                    if (key.isAcceptable()) {
-                        SocketChannel clientChannel = serverChannel.accept();
-                        clientChannel.configureBlocking(false);
-
-                        SSLEngine engine = SecureSocketConfig.config();
-
-                        ConnectionContext ctx = new ConnectionContext(engine);
-
-                        System.out.println("[INFO] Handshake is beginning");
-                        ctx.engine.beginHandshake();
-
-                        var ops = SelectionKey.OP_READ;
-
-                        if (ctx.engine.getHandshakeStatus() == SSLEngineResult.HandshakeStatus.NEED_WRAP) {
-                            ops = SelectionKey.OP_WRITE;
-                        }
-
-                        clientChannel.register(selector, ops, ctx);
-                    }
-
-                    if (key.isReadable() || key.isWritable()) {
-                        ConnectionContext ctx = (ConnectionContext) key.attachment();
-                        SSLEngine engine = ctx.engine;
-
-                        if (!ctx.handshakeDone) {
-                            SecureSocketConfig.doHandshake(key);
-                            SSLEngineResult.HandshakeStatus hs = engine.getHandshakeStatus();
-                            if (hs == SSLEngineResult.HandshakeStatus.FINISHED || hs == SSLEngineResult.HandshakeStatus.NOT_HANDSHAKING)
-                                ctx.handshakeDone = true;
-                        }
-
-                        if (ctx.handshakeDone) {
-                            System.out.println("[INFO] Handshake completed with success");
-                            SocketChannel clientChannel = (SocketChannel) key.channel();
-
-                            ctx.peerNetData.clear();
-                            int n = clientChannel.read(ctx.peerNetData);
-
-                            if (n < 0) {
-                                ctx = null;
-                                clientChannel.close();
-                                key.cancel();
-                                return;
-                            }
-
-                            if (n == 0) {
-                                continue;
-                            }
-
-                            ctx.peerNetData.flip();
-                            var res = engine.unwrap(ctx.peerNetData, ctx.peerAppData);
-                            ctx.peerNetData.clear();
-
-                            if (ctx.peerAppData.hasRemaining() && res.getStatus() == SSLEngineResult.Status.OK) {
-                                var temp = new ByteArrayInputStream(ctx.peerAppData.array());
-                                var result = new BufferedReader(new InputStreamReader(temp, StandardCharsets.UTF_8));
-                                SyslogMessage msg = SyslogMessage.createFromString(result.readLine());
-
-                                writeLog(msg);
-
-                                String response = "OK/n";
-                                ctx.myAppData = ByteBuffer.wrap(response.getBytes());
-                                ctx.myAppData.flip();
-
-                                engine.wrap(ctx.myAppData, ctx.myNetData);
-                                ctx.myAppData.clear();
-
-                                ctx.myNetData.flip();
-                                clientChannel.write(ctx.myNetData);
-                                ctx.myNetData.clear();
-
-
-                                engine.closeOutbound();
-                                engine.wrap(ByteBuffer.allocate(0), ctx.myNetData);
-                                ctx.myNetData.flip();
-                                clientChannel.write(ctx.myNetData);
-                                ctx.myNetData.clear();
-
-                                ctx.peerNetData.clear();
-                                ctx.peerAppData.clear();
-                                ctx.myAppData.clear();
-                                ctx.myNetData.clear();
-
-                                clientChannel.close();
-                                key.cancel();
-
-                                System.out.println("[INFO] Connection closed with a success operation\n==================   ");
-                            }
-                        }
-                    }
-                }
+                SSLSocket client = (SSLSocket) server.accept();
+                pool.submit(() -> runConnection(client));
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void runConnection(SSLSocket client) {
+        try {
+            InputStream input = client.getInputStream();
+            BufferedReader req = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+
+            String msg;
+            while((msg = req.readLine()) != null) {
+                SyslogMessage log = SyslogMessage.createFromString(msg);
+                writeLog(log);
+
+                String response = "OK\r\n";
+                OutputStream out = client.getOutputStream();
+                out.write(response.getBytes(StandardCharsets.UTF_8));
+                out.flush();
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/me/kpsantiago/config/SecureSocketConfig.java
+++ src/main/java/me/kpsantiago/config/SecureSocketConfig.java
@@ -1,195 +1,33 @@
 package me.kpsantiago.config;
 
-import lombok.Value;
-
-import javax.net.ssl.*;
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.SocketChannel;
 import java.security.KeyStore;
 
 public class SecureSocketConfig {
     private static final String password = System.getenv("SECRET_PASS");
 
-    public static SSLEngine config() throws Exception {
+    public static ServerSocketFactory config() throws Exception {
         char[] passphrase = password.toCharArray();
 
-        // Key Material -> identify the server
         KeyStore ksKeys = KeyStore.getInstance("JKS");
         ksKeys.load(new FileInputStream("serverkeystore.jks"), passphrase);
 
-        // Trust Material -> identify the ctx.peers
         KeyStore ksTrust = KeyStore.getInstance("JKS");
         ksTrust.load(new FileInputStream("servertruststore.jks"), passphrase);
 
-        // KeyManagers decide which key material to use
         KeyManagerFactory kmf = KeyManagerFactory.getInstance("PKIX");
         kmf.init(ksKeys, passphrase);
 
-        // Trust Manager Factory decides whether to allow connection
         TrustManagerFactory tmf = TrustManagerFactory.getInstance("PKIX");
         tmf.init(ksTrust);
 
-        // Initiate SSLContext to use TLS
-        // Pass the key and trust material to context know how to authenticate
         SSLContext sslContext = SSLContext.getInstance("TLS");
         sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
 
-        // Initiate SSLEngine
-        SSLEngine engine = sslContext.createSSLEngine();
-        engine.setNeedClientAuth(true);
-        engine.setUseClientMode(false);
-
-        // return ssl engine
-        return engine;
-    }
-
-    public static void doHandshake(SelectionKey key) throws SSLException, IOException {
-        SocketChannel channel = (SocketChannel) key.channel();
-        ConnectionContext ctx = (ConnectionContext) key.attachment();
-        SSLEngine engine = ctx.engine;
-
-        SSLEngineResult.HandshakeStatus hs = engine.getHandshakeStatus();
-
-        switch (hs) {
-            case NEED_UNWRAP: {
-                int n = channel.read(ctx.peerNetData);
-                if (n < 0) {
-                    engine.closeOutbound();
-                    channel.close();
-                    key.cancel();
-                    return;
-                }
-
-                if (n == 0) {
-                    key.interestOps(SelectionKey.OP_READ);
-                    return;
-                }
-
-                ctx.peerNetData.flip();
-                SSLEngineResult res = engine.unwrap(ctx.peerNetData, ctx.peerAppData);
-                ctx.peerNetData.compact();
-
-                handleKeyOperation(key, engine, res);
-
-                switch (res.getStatus()) {
-                    case OK:
-                        break;
-                    case BUFFER_OVERFLOW:
-                        ctx.peerAppData = enlargeBuffer(ctx.peerAppData, engine.getSession().getApplicationBufferSize());
-                        break;
-                    case BUFFER_UNDERFLOW:
-                        int needed = engine.getSession().getPacketBufferSize();
-                        if (ctx.peerNetData.capacity() < needed) {
-                            ctx.peerNetData = enlargeBuffer(ctx.peerNetData, needed);
-                        }
-                        return;
-                    case CLOSED:
-                        System.out.println("[INFO] Handshake closed: connection closed.");
-                        engine.closeOutbound();
-                        channel.close();
-                        key.cancel();
-                        break;
-                }
-                break;
-            }
-
-            case NEED_WRAP: {
-                ctx.myNetData.clear();
-                SSLEngineResult res = engine.wrap(ctx.myAppData, ctx.myNetData);
-
-                handleKeyOperation(key, engine, res);
-
-                switch (res.getStatus()) {
-                    case OK:
-                        ctx.myNetData.flip();
-                        channel.write(ctx.myNetData);
-
-                        if (ctx.myNetData.hasRemaining()) {
-                            key.interestOps(SelectionKey.OP_WRITE);
-                        }
-
-                        break;
-                    case BUFFER_OVERFLOW:
-                        ctx.myNetData = enlargeBuffer(ctx.myNetData, engine.getSession().getPacketBufferSize());
-                        break;
-                    case CLOSED:
-                        System.out.println("[INFO] Handshake closed: connection closed.");
-                        engine.closeOutbound();
-                        channel.close();
-                        key.cancel();
-                        break;
-                }
-                break;
-            }
-            case NEED_UNWRAP_AGAIN: {
-                ctx.peerNetData.flip();
-                SSLEngineResult res = engine.unwrap(ctx.peerNetData, ctx.peerAppData);
-                ctx.peerNetData.compact();
-
-                handleKeyOperation(key, engine, res);
-
-                switch (res.getStatus()) {
-                    case OK:
-                        break;
-                    case BUFFER_OVERFLOW:
-                        ctx.peerAppData = enlargeBuffer(ctx.peerAppData, engine.getSession().getApplicationBufferSize());
-                        break;
-                    case BUFFER_UNDERFLOW:
-                        int needed = engine.getSession().getPacketBufferSize();
-                        if (ctx.peerNetData.capacity() < needed) {
-                            ctx.peerNetData = enlargeBuffer(ctx.peerNetData, needed);
-                        }
-                        return;
-                    case CLOSED:
-                        System.out.println("[INFO] Handshake closed: connection closed.");
-                        engine.closeOutbound();
-                        key.cancel();
-                        channel.close();
-                        break;
-                }
-                break;
-            }
-            case NOT_HANDSHAKING:
-            case FINISHED:
-                key.interestOps(SelectionKey.OP_READ);
-                break;
-        }
-    }
-
-    private static void handleKeyOperation(SelectionKey key, SSLEngine engine, SSLEngineResult res) {
-        switch (res.getHandshakeStatus()) {
-            case NEED_UNWRAP,
-                 NEED_UNWRAP_AGAIN,
-                 FINISHED,
-                 NOT_HANDSHAKING:
-                key.interestOps(SelectionKey.OP_READ);
-                break;
-            case NEED_WRAP:
-                key.interestOps(SelectionKey.OP_WRITE);
-                break;
-            case NEED_TASK:
-                Runnable task;
-                while ((task = engine.getDelegatedTask()) != null) {
-                    task.run();
-                }
-                var hs = engine.getHandshakeStatus();
-                if (hs == SSLEngineResult.HandshakeStatus.NEED_WRAP) {
-                    key.interestOps(SelectionKey.OP_WRITE);
-                } else if (hs == SSLEngineResult.HandshakeStatus.NEED_UNWRAP) {
-                    key.interestOps(SelectionKey.OP_READ);
-                }
-                break;
-        }
-    }
-
-
-    private static ByteBuffer enlargeBuffer(ByteBuffer buffer, int newCapacity) {
-        ByteBuffer newBuffer = ByteBuffer.allocate(newCapacity);
-        buffer.flip();
-        newBuffer.put(buffer);
-        return newBuffer;
+        return sslContext.getServerSocketFactory();
     }
 }
